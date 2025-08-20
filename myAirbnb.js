68812ba1400dca74d06f5ce59c471942985b15bf
IBK-Tim <ibukunanifowose05@gmial.com>
2025-08-20 13:05:44 +0100

    first commit

---
+++ myAirbnb.js
@@ -0,0 +1,14 @@
+//   const tabs = document.querySelectorAll(".scroll2 div");
+//   const contents = document.querySelectorAll(".content");
+
+//   tabs.forEach(tab => {
+//     tab.addEventListener("click", () => {
+//       // Remove active from all tabs and contents
+//       tabs.forEach(t => t.classList.remove("active"));
+//       contents.forEach(c => c.classList.remove("active"));
+
+//       // Add active to clicked tab and matching content
+//       tab.classList.add("active");
+//       document.getElementById(tab.getAttribute("data-tab")).classList.add("active");
+//     });
+//   });
